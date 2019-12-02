2ad49a8fe514cade26a017b0fb84ad1b4df21d27
anzhelika <anzelikasht@gmail.com>
2019-12-03 00:48:32 +0300

    add homewokr20191202

--- src/homework20191202/Task.java
+++ src/homework20191202/Task.java
@@ -1,5 +1,27 @@
-package homework02122019;
+package homework20191202;
 
 public class Task {
 
+    public static void reselveAcq(float a, float b, float c){
+        float x1;
+        float x2;
+        float discriminant = b * b - 4 * a * c;
+        if(discriminant < 0){
+            System.out.println("no roots");
+        }else if(discriminant == 0){
+            x1 = x2 = -b / 2*a;
+            System.out.println("x1 = x2 = " + x1);
+        }else{
+            x1 = (float) ((-b + Math.sqrt(discriminant)) / 2*a);
+            x2 = (float) ((-b - Math.sqrt(discriminant)) / 2*a);
+            System.out.println("x1 = " + x1 + "\nx2 = " + x2);
+        }
+    }
+
+    public static void main(String[] args) {
+        reselveAcq(1, 2, 3);
+        reselveAcq(1, 2, 1);
+        reselveAcq(1, -3, -4);
+
+    }
 }
