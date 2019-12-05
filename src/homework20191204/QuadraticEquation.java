f5264603935ec504ef9447e7d2a32928f370431e
anzhelika <anzelikasht@gmail.com>
2019-12-05 12:17:56 +0300

    create method 'public resolveAcq()'

--- src/homework20191204/QuadraticEquation.java
+++ src/homework20191204/QuadraticEquation.java
@@ -10,8 +10,6 @@ public class QuadraticEquation {
         this.a = a;
         this.b = b;
         this.c = c;
-        roots = new Roots();
-        resolveAcq(a, b, c);
     }
 
     private void resolveAcq(float a, float b, float c) {
@@ -19,6 +17,11 @@ public class QuadraticEquation {
         if (discriminant >= 0) {
             roots.calculatingRoots(a, b, c, discriminant);
         }
+    }
+
+    public void resolveAcq() {
+        roots = new Roots();
+        resolveAcq(a, b, c);
     }
 
     public Float getRootOne() {
@@ -42,14 +45,17 @@ public class QuadraticEquation {
 
     public static void main(String[] args) {
         QuadraticEquation quadraticEquation1 = new QuadraticEquation(1, -3, -4);
+        quadraticEquation1.resolveAcq();
         System.out.println(quadraticEquation1);
         System.out.println("root one: " + quadraticEquation1.getRootOne());
         System.out.println("root two: " + quadraticEquation1.getRootTwo());
         QuadraticEquation quadraticEquation2 = new QuadraticEquation(1, 2, 1);
+        quadraticEquation2.resolveAcq();
         System.out.println(quadraticEquation2);
         System.out.println("root one: " + quadraticEquation2.getRootOne());
         System.out.println("root two: " + quadraticEquation2.getRootTwo());
         QuadraticEquation quadraticEquation3 = new QuadraticEquation(1, 2, 3);
+        quadraticEquation3.resolveAcq();
         System.out.println(quadraticEquation3);
         System.out.println("root one: " + quadraticEquation3.getRootOne());
         System.out.println("root two: " + quadraticEquation3.getRootTwo());
