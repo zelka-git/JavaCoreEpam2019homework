d8002ec37e97174c35844b6eaa80a8546b23e842
anzhelika <anzelikasht@gmail.com>
2019-12-08 00:06:47 +0300

    add enum CategoryGoods

---
+++ src/homework20191206/CategoryGoods.java
@@ -0,0 +1,5 @@
+package homework20191206;
+
+public enum CategoryGoods {
+    FOOD, ANIMALS, MEDICINES, FRAGILE
+}
--- src/homework20191206/Goods.java
+++ src/homework20191206/Goods.java
@@ -4,4 +4,5 @@ public class Goods {
     private String name;
     private int weight;
     private int space;
+    private CategoryGoods category;
 }
