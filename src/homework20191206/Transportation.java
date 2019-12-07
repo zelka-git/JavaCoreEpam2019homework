bea4ba520ed435b01a47ed2c87514241027be607
anzhelika <anzelikasht@gmail.com>
2019-12-08 00:38:46 +0300

    add fields palaceFrom, placeTo, price

--- src/homework20191206/Transportation.java
+++ src/homework20191206/Transportation.java
@@ -2,6 +2,9 @@ package homework20191206;
 
 public class Transportation {
     private Goods[] goods;
-    private  TransportCompany transportCompany;
+    private TransportCompany transportCompany;
+    private String placeFrom;
+    private String placeTo;
+    private int price;
 
 }
