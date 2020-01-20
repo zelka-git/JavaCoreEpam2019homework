cc02e4eff62e7b46ad76650d31ae397380eaec97
anzhelika <anzelikasht@gmail.com>
2020-01-20 14:35:02 +0300

    change testing methods

--- src/main/java/ru/epam/javacore/homework20200113/application/Application.java
+++ src/main/java/ru/epam/javacore/homework20200113/application/Application.java
@@ -5,101 +5,120 @@ import ru.epam.javacore.homework20200113.application.serviceholder.StorageType;
 import ru.epam.javacore.homework20200113.cargo.service.CargoService;
 import ru.epam.javacore.homework20200113.cargo.service.TypeSortCargo;
 import ru.epam.javacore.homework20200113.carrier.service.CarrierService;
-import ru.epam.javacore.homework20200113.common.business.exception.checked.InitStorageException;
-import ru.epam.javacore.homework20200113.common.business.exception.checked.PrintStorageException;
-import ru.epam.javacore.homework20200113.common.business.exception.checked.ReportException;
 import ru.epam.javacore.homework20200113.common.solutions.utils.ArrayUtils;
 import ru.epam.javacore.homework20200113.reporting.ReportDefaultService;
 import ru.epam.javacore.homework20200113.reporting.ReportService;
 import ru.epam.javacore.homework20200113.storage.initor.InitStorageType;
 import ru.epam.javacore.homework20200113.storage.initor.StorageInitor;
 import ru.epam.javacore.homework20200113.storage.initor.StorageInitorFactory;
-import ru.epam.javacore.homework20200113.storage.print.PrintToTextFile;
 import ru.epam.javacore.homework20200113.transportation.service.TransportationService;
 
-import java.io.File;
-import java.io.IOException;
+import java.util.Collection;
 
 public class Application {
 
+    private static final String SEPARATOR = "--------------";
     private static CargoService cargoService;
     private static CarrierService carrierService;
     private static TransportationService transportationService;
 
     public static void main(String[] args) {
-        ServiceHolder.initServiceHolder(StorageType.COLLECTION);
-        ServiceHolder storage = ServiceHolder.getInstance();
-        cargoService = storage.getCargoService();
-        carrierService = storage.getCarrierService();
-        transportationService = storage.getTransportationService();
-        StorageInitor storageInitor;
-
-        File file = new File("/ru/epam/javacore/homework20200113/input_xml.xml");
-        File file1 = new File("C:\\Anzhelika\\projects\\ideaProjects\\JavaCore\\src\\main\\resources\\ru\\epam\\javacore\\homework20200113\\input_xml.xml");
-        System.out.println(file.exists());
-        System.out.println(file1.exists());
-
-
-
-        storageInitor = StorageInitorFactory.getStorageInitor(InitStorageType.XML_SAX_FILE);
-
         try {
+
+
+            ServiceHolder.initServiceHolder(StorageType.COLLECTION);
+            ServiceHolder storage = ServiceHolder.getInstance();
+            cargoService = storage.getCargoService();
+            carrierService = storage.getCarrierService();
+            transportationService = storage.getTransportationService();
+
+            StorageInitor storageInitor;
+            storageInitor = StorageInitorFactory.getStorageInitor(InitStorageType.XML_SAX_FILE);
             storageInitor.initStorage();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (InitStorageException e) {
-            e.printStackTrace();
+
+            printStorage();
+
+            demoSearchOperations();
+            demoTestSortCargo();
+            demoTestException();
+
+
+            ReportService reportService = new ReportDefaultService(
+                    cargoService, carrierService, transportationService
+            );
+            reportService.exportData();
+
         } catch (Exception e) {
             e.printStackTrace();
         }
 
-        printStorage();
-
-        ReportService reportService = new ReportDefaultService(
-                cargoService, carrierService, transportationService
-        );
-        try {
-            reportService.exportData();
-        } catch (ReportException e) {
-            e.printStackTrace();
-        }
-
-        PrintToTextFile printToTextFile = new PrintToTextFile();
-        try {
-            printToTextFile.printStorage();
-        } catch (PrintStorageException e) {
-            e.printStackTrace();
-        }
-//        testSortCargo();
-
-//        System.out.println("\ntest delete by Id ");
-////        System.out.println(cargoService.deleteById(null));
-//        System.out.println(cargoService.deleteById(Long.valueOf(1)));
-//        cargoService.printAll();
-
     }
 
-
-    private static void testSortCargo() {
-        System.out.println("Begin test sort:");
-        System.out.println("Sort by weight");
-        ArrayUtils.printArray(cargoService.getAllSortedItems(TypeSortCargo.WEIGHT));
-        System.out.println("________________________");
-        System.out.println("Sort by name");
-        ArrayUtils.printArray(cargoService.getAllSortedItems(TypeSortCargo.NAME));
-        System.out.println("________________________");
-        System.out.println("Sort by weight name");
-        ArrayUtils.printArray(cargoService.getAllSortedItems(TypeSortCargo.WEIGHT_NAME));
-        System.out.println("________________________");
-        System.out.println("Sort by name weight");
-        ArrayUtils.printArray(cargoService.getAllSortedItems(TypeSortCargo.NAME_WEIGHT));
-        System.out.println("________________________");
-    }
 
     private static void printStorage() {
         cargoService.printAll();
         carrierService.printAll();
         transportationService.printAll();
+    }
+
+    private static void demoSearchOperations() {
+        System.out.println();
+        System.out.println("SEARCH CARGO BY ID = 1");
+        System.out.println(cargoService.getById(1L));
+        printSeparator();
+
+        System.out.println("SEARCH CARRIER BY ID = 6");
+        System.out.println(carrierService.getById(6L));
+        printSeparator();
+
+        System.out.println("SEARCH CARGOES BY NAME = 'Banana'");
+        printCollection(cargoService.getByName("Banana"));
+        printSeparator();
+
+        System.out.println("SEARCH CARRIERS BY NAME = 'Slow delivery'");
+        printCollection(carrierService.getByName("Slow delivery"));
+    }
+
+    private static void printSeparator() {
+        System.out.println(SEPARATOR);
+    }
+
+    private static void printCollection(Collection<?> collection) {
+        for (Object obj : collection) {
+            System.out.println(obj.toString());
+        }
+    }
+
+
+    private static void demoTestSortCargo() {
+        System.out.println("\nBegin test sort:");
+        System.out.println("Sort by weight");
+        ArrayUtils.printArray(cargoService.getAllSortedItems(TypeSortCargo.WEIGHT));
+        printSeparator();
+        System.out.println("Sort by name");
+        ArrayUtils.printArray(cargoService.getAllSortedItems(TypeSortCargo.NAME));
+        printSeparator();
+        System.out.println("Sort by weight name");
+        ArrayUtils.printArray(cargoService.getAllSortedItems(TypeSortCargo.WEIGHT_NAME));
+        printSeparator();
+        System.out.println("Sort by name weight");
+        ArrayUtils.printArray(cargoService.getAllSortedItems(TypeSortCargo.NAME_WEIGHT));
+        printSeparator();
+    }
+
+    private static void demoTestException() {
+        System.out.println("\nTEST DELETE BY Id ");
+        System.out.println("Delete by Id = null");
+        System.out.println("result = " + cargoService.deleteById(null));
+        printSeparator();
+        try {
+            System.out.println("Delete by Id = 1");
+            System.out.println("result = " + cargoService.deleteById(1L));
+            printSeparator();
+        }catch (Exception e){
+            System.out.println("Exception: can't delete cargo");
+            System.out.println(e.getMessage());
+        }
     }
 
 
