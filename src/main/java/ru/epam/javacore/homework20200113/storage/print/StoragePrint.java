9031267a4b929f986afb0ef1f819ea57ea064530
anzhelika <anzelikasht@gmail.com>
2020-01-20 14:30:01 +0300

    deleted files

--- src/main/java/ru/epam/javacore/homework20200113/storage/print/StoragePrint.java
+++
@@ -1,7 +0,0 @@
-package ru.epam.javacore.homework20200113.storage.print;
-
-import ru.epam.javacore.homework20200113.common.business.exception.checked.PrintStorageException;
-
-public interface StoragePrint {
-    void printStorage() throws PrintStorageException;
-}
