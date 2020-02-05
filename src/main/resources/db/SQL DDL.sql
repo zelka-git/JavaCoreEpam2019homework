a3ed977a0784847b2633960baa2e298fd92f4c28
anzhelika <anzelikasht@gmail.com>
2020-02-05 23:31:22 +0300

    date -> timestamp

--- src/main/resources/db/SQL DDL.sql
+++ src/main/resources/db/SQL DDL.sql
@@ -1,36 +1,33 @@
 
-CREATE table CARGOS
-(
-id INT NOT null,
+create table if not exists cargos(
+id BIGINT NOT null,
 name varchar(50) NOT null,
 weight INT NOT null,
 cargoType varchar(50) NOT null, 
 size varchar(50), 
 material varchar(50),
 description varchar(200), 
-expiratonDate date, 
+expiratonDate timestamp,
 storeTemperature int,
 primary key (id)
 );
 
 
-CREATE table CARRIERS
-(
-id INT NOT null,
+create table if not exists carriers(
+id BIGINT NOT null,
 name varchar(50) NOT null,
 address varchar(50) NOT null,
 carrierType varchar(50) NOT null,  
 primary key (id)
 );
 
-CREATE table TRANSPORTATIONS
-(
-id INT NOT null,
-id_cargo INT NOT null,
-id_carrier INT NOT null, 
+create table if not exists transportations(
+id BIGINT NOT null,
+id_cargo BIGINT NOT null,
+id_carrier BIGINT NOT null,
 description varchar(300),
 billTo varchar(100),
-date date NOT null,
+date timestamp NOT null,
 primary key (id),
 foreign key (id_cargo) references CARGOS(id),
 foreign key (id_carrier) references CARRIERS(id)
--- src/main/resources/db/SQL DML.sql
+++ src/main/resources/db/SQL DML.sql
@@ -1,18 +1,18 @@
 insert into CARGOS 
 (id,  name, weight, cargoType, expiratonDate, storeTemperature)
-Values
-(1, 'Banana', 123, 'FOOD', '2013-10-25', 15),
-(2, 'Apple', 222, 'FOOD', '2015-10-25', 15);
+values
+(1, 'Banana', 123, 'FOOD', parsedatetime('20-09-2025 18:47', 'dd-MM-yyyy hh:mm'), 15),
+(2, 'Apple', 222, 'FOOD', parsedatetime('20-09-2025 18:47', 'dd-MM-yyyy hh:mm'), 15);
 
 insert into CARGOS 
 (id,  name, weight, cargoType, size, material)
-Values
+values
 (3, 'Dress', 321, 'CLOTHES', 'M', 'cotton'),
 (4, 'Skirt', 213, 'CLOTHES', 'S', 'wool');
 
 insert into CARGOS 
 (id, name, weight, cargoType, description)
-Values
+values
 (5, 'HP', 111, 'COMPUTERS', 'this is a computer 1'),
 (6, 'Asus', 241, 'COMPUTERS', 'this is a computer 2');
 
@@ -26,10 +26,10 @@ Values
 
 insert into TRANSPORTATIONS 
 (id, id_cargo, id_carrier, description, billTo, date)
-Values
-(1, 1, 1, 'this transportation 1', 'P. N. Ivanov', '2009-10-25'),
-(2, 2, 2, 'this transportation 2', 'U. N. Romanov', '2010-10-25'),
-(3, 3, 3, 'this transportation 3', 'I. D. Titov', '2012-10-25'),
-(4, 4, 1, 'this transportation 4', 'P. N. Ivanov', '2009-10-25'),
-(5, 5, 2, 'this transportation 5', 'U. N. Romanov', '2010-10-25'),
-(6, 6, 3, 'this transportation 6', 'I. D. Titov', '2012-10-25');
+values
+(1, 1, 1, 'this transportation 1', 'P. N. Ivanov', parsedatetime('20-09-2025 18:47', 'dd-MM-yyyy hh:mm')),
+(2, 2, 2, 'this transportation 2', 'U. N. Romanov', parsedatetime('20-09-2025 18:47', 'dd-MM-yyyy hh:mm')),
+(3, 3, 3, 'this transportation 3', 'I. D. Titov', parsedatetime('20-09-2025 18:47', 'dd-MM-yyyy hh:mm')),
+(4, 4, 1, 'this transportation 4', 'P. N. Ivanov', parsedatetime('20-09-2025 18:47', 'dd-MM-yyyy hh:mm')),
+(5, 5, 2, 'this transportation 5', 'U. N. Romanov', parsedatetime('20-09-2025 18:47', 'dd-MM-yyyy hh:mm')),
+(6, 6, 3, 'this transportation 6', 'I. D. Titov', parsedatetime('20-09-2025 18:47', 'dd-MM-yyyy hh:mm'));
